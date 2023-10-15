38e241d9a357202600df91bd74b18671e421f47a
Mian Shoaib <128806915+Shoaibtalib611@users.noreply.github.com>
2023-10-15 13:36:36 +0500

    Update To_do_List (variable,list, user input, if else statement, dictionary,loop).py

--- Project 1/To_do_List (variable,list, user input, if else statement, dictionary,loop).py
+++ Project 1/To_do_List (variable,list, user input, if else statement, dictionary,loop).py
@@ -1,5 +1,5 @@
 tasks = []
-# Main Loop
+
 while True:
     print("Todo List Manager")
     print("1. Add Task")
@@ -9,12 +9,14 @@ while True:
     print("5. Exit")
     
     choice = input("Enter your choice: ")
-    # Add Task
+    
+ 
     if choice == "1":
         task_name = input("Enter task name: ")
         tasks.append({"task": task_name, "completed": False})
         print("Task added successfully!\n")
-    # Mark Task as Completed 
+    
+    
     elif choice == "2":
         task_index = int(input("Enter the task number you want to mark as completed: "))
         if task_index >= 1 and task_index <= len(tasks):
@@ -23,7 +25,7 @@ while True:
         else:
             print("Invalid task number. Please try again.\n")
     
-    # View Tasks
+    
     elif choice == "3":
         print("Tasks:")
         for index, task in enumerate(tasks, start=1):
@@ -31,7 +33,7 @@ while True:
             print(f"{index}. {task['task']} - {status}")
         print()
     
-    # Delete Task
+  
     elif choice == "4":
         task_index = int(input("Enter the task number you want to delete: "))
         if task_index >= 1 and task_index <= len(tasks):
@@ -40,11 +42,10 @@ while True:
         else:
             print("Invalid task number. Please try again.\n")
     
-    # Exit the program
+   
     elif choice == "5":
         print("Exiting Todo List Manager. Goodbye!")
         break
     
-    # Invalid choice
     else:
         print("Invalid choice. Please enter a valid option.\n")
