5a450a8f648ed0094f048c0e95a7ef9136ba1612
Mian Shoaib <128806915+Shoaibtalib611@users.noreply.github.com>
2023-10-20 21:48:11 +0500

    Update test1.py

--- Testing/test1.py
+++ Testing/test1.py
@@ -2,4 +2,4 @@ def get_full_name(first, last):
     full_name = "{0} {1}".format(first, last)
     return full_name.title()
 
-print(get_full_name('Kumail', 'Haider'))
+print(get_full_name('Shoaib', 'Talib'))
